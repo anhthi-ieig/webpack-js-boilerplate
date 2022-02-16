836a0fc5b80febe015f4c70bb33e3a110bb43f1a
Thi Nguyen <anhthi.ieig@gmail.com>
2022-02-16 15:35:46 +0700

    Update package.json

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,7 +5,7 @@ import styles from './home.scss';
 const Home = () => {
   return (
     <div className={styles.home}>
-      <p className={styles.title}>This is home page</p>
+      <p className={styles.pageTitle}>This is home page</p>
     </div>
   );
 };
